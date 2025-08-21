b884875b47df66008b0a3e90ebde36eed295667b
Jordan Hans <jordan.hans@Jordans-MacBook-Air.local>
2025-08-21 14:51:54 -0500

    Issue #27: Phase 2 - Pattern matching implementations with similarity algorithms

---
+++ src/deduplication/patterns/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Pattern Matchers Module
+ * 
+ * Exports for all pattern matching implementations
+ * 
+ * @module patterns
+ */
+
+export { StackTracePattern } from './StackTracePattern'
+export { ErrorMessagePattern } from './ErrorMessagePattern'
+export { ConsoleOutputPattern } from './ConsoleOutputPattern'
+export { AssertionPattern } from './AssertionPattern'
+
+// Re-export pattern matcher interface
+export type { IPatternMatcher } from '../../types/deduplication'
